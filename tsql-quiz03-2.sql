e235a3c8b160c25f1390f7d600a2b66ce81787e6
AJ McCray <william.mccray1@outlook.com>
2020-02-18 17:54:12 -0500

    quiz 3 final

--- tsql-quiz03-2.sql
+++ tsql-quiz03-2.sql
@@ -1,11 +1,33 @@
+.echo on
+.headers on
+-- Name: AJ McCray
+-- File: tsql-quiz03
+-- Date: February 4, 2020
+
 /* 1. Write a report giving the employee id, first name, last name, and the average amount of all orders by
 that employee. */
-select employeeid, FirstName + ' ' + LastName, avgorders
+select o.EmployeeID, FirstName + ' ' + LastName as Employee, avg(unitprice * Quantity)
+from Orders o
+join Employees e
+on o.employeeid = e.employeeid
+join "Order Details" od
+on o.orderid = od.orderid
+group by o.EmployeeID, FirstName, LastName;
 
 /* 2. Write a report that lists our product categories and the average price of all products of that category.
 Include the category id, the category name, and the average price. */
-
+select c.CategoryID, categoryname, avg(unitprice)
+from categories c
+join products p
+on c.CategoryID = p.CategoryID
+group by c.CategoryID, c.CategoryName;
 
 /* 3. In some cities, we have customers, suppliers, and employees. Write a report that lists the customer’s
 contact name, the supplier’s contact name, the employee’s name, and the city in which all three are
 located. */
+select c.contactname as 'Customer Contact', s.contactname as 'Supplier Contact', firstname + ' ' + lastname as Employee, c.city
+from Customers c
+join Employees e
+on c.city = e.city
+join Suppliers s
+on s.city = c.city;
--- tsql-quiz03.sql
+++ tsql-quiz03.sql
@@ -1,10 +1,33 @@
-select distinct RegionDescription, LastName + ', ' + FirstName as Employee, S.CompanyName as Supplier, ProductName from Orders as O
-	join [Order Details] as OD on O.OrderID = OD.OrderID
-	join Products as P on OD.ProductID = P.ProductID
-	join Employees as E on E.EmployeeID = O.EmployeeID
-	join Suppliers as S on P.SupplierID = S.SupplierID
-	join EmployeeTerritories as ET on E.EmployeeID = ET.EmployeeID
-	join Territories as T on ET.TerritoryID = T.TerritoryID
-	join Region as R on R.RegionID = T.RegionID
-	where S.Country = 'Germany'
-	order by RegionDescription;
+.echo on
+.headers on
+-- Name: AJ McCray
+-- File: tsql-quiz03
+-- Date: February 4, 2020
+
+/* 1. Write a report giving the employee id, first name, last name, and the average amount of all orders by
+that employee. */
+select o.EmployeeID, FirstName || ' ' || LastName as Employee, avg(unitprice * Quantity) as AverageOrders
+from Orders o
+join Employees e
+on o.employeeid = e.employeeid
+join order_details od
+on o.orderid = od.orderid
+group by o.EmployeeID, FirstName, LastName;
+
+/* 2. Write a report that lists our product categories and the average price of all products of that category.
+Include the category id, the category name, and the average price. */
+select c.CategoryID, categoryname, avg(unitprice) as AveragePrice
+from categories c
+join products p
+on c.CategoryID = p.CategoryID
+group by c.CategoryID, c.CategoryName;
+
+/* 3. In some cities, we have customers, suppliers, and employees. Write a report that lists the customer’s
+contact name, the supplier’s contact name, the employee’s name, and the city in which all three are
+located. */
+select c.contactname as 'Customer Contact', s.contactname as 'Supplier Contact', firstname || ' ' || lastname as Employee, c.city
+from Customers c
+join Employees e
+on c.city = e.city
+join Suppliers s
+on s.city = c.city;
